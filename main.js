73dc7db283dc8afa80d899c92a2f1d1d0a5cac44
KimMinme <128785019+KimMinme@users.noreply.github.com>
2023-07-14 21:11:45 +0900

    App - 홈페이지 구현
    - query string이 없을 때 홈페이지를 띄우도록 하였다.

--- main.js
+++ main.js
@@ -5,35 +5,62 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-		// pathname을 변수 pathname에 저장
     var pathname = url.parse(_url, true).pathname;
-    var title = queryData.id;
 
-    if(pathname === '/'){ // pathname이 root일 경우 페이지 보여줌
-      fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-        var template = `
-      <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        <ul>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ul>
-        <h2>${title}</h2>
-        <p>${description}</p>
-      </body>
-      </html>
-      `;
-      response.writeHead(200);
-      response.end(template);
-      });
-    } else{ // 아닐 경우 Not Found라는 메시지를 띄움
+    if(pathname === '/'){
+      if(queryData.id === undefined){ // query string이 없을 때
+        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){ // 필요 없음
+          var title = 'Welcome'; // title을 'Welcome'으로
+          var description = 'Hello, Node.js'; // description을 'Hello, Node.js'로
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ul>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `;
+          response.writeHead(200);
+          response.end(template);
+        }); // 필요 없음
+      } else{ // query string이 있을 때
+        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+          var title = queryData.id;
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ul>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `;
+          response.writeHead(200);
+          response.end(template);
+        });
+      }
+    } else{
       response.writeHead(404);
       response.end('Not found');
     }
