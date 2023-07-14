5868d81c9f5d0804112def4abcc1aa3deff0d756
KimMinme <128785019+KimMinme@users.noreply.github.com>
2023-07-14 21:06:45 +0900

    App - 동적인 웹페이지 만들기
    - 웹페이지의 title을 동적으로 만들었다.

--- main.js
+++ main.js
@@ -1,18 +1,42 @@
 var http = require('http');
 var fs = require('fs');
-var url = require('url'); // url 모듈
+var url = require('url');
 
 var app = http.createServer(function(request,response){
-    var _url = request.url; // url을 요청해서 _url에 저장
-    var queryData = url.parse(_url, true).query; // query string 가져오기
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    var title = queryData.id; // queryData.id에 의미있는 이름 붙이기
     if(_url == '/'){
-      _url = '/index.html';
+      title = 'Welcome'; // url이 '/'일 때, title을 welcome으로
     }
     if(_url == '/favicon.ico'){
       return response.writeHead(404);
     }
     response.writeHead(200);
-    response.end(queryData.id); // queryData.id를 보여줌
- 
+		// template이 페이지 소스 변수임
+		// title을 동적으로 변환시킴
+    var template = `
+    <!doctype html>
+		<html>
+		<head>
+		  <title>WEB1 - ${title}</title>
+		  <meta charset="utf-8">
+		</head>
+		<body>
+		  <h1><a href="/">WEB</a></h1>
+		  <ul>
+		    <li><a href="/?id=HTML">HTML</a></li>
+		    <li><a href="/?id=CSS">CSS</a></li>
+		    <li><a href="/?id=JavaScript">JavaScript</a></li>
+		  </ul>
+		  <h2>${title}</h2>
+		  <p><a href="https://www.w3.org/TR/html5/" target="_blank" title="html5 speicification">Hypertext Markup Language (HTML)</a> is the standard markup language for <strong>creating <u>web</u> pages</strong> and web applications.Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
+		  <img src="coding.jpg" width="100%">
+		  </p><p style="margin-top:45px;">HTML elements are the building blocks of HTML pages. With HTML constructs, images and other objects, such as interactive forms, may be embedded into the rendered page. It provides a means to create structured documents by denoting structural semantics for text such as headings, paragraphs, lists, links, quotes and other items. HTML elements are delineated by tags, written using angle brackets.
+		  </p>
+		</body>
+		</html>
+    `;
+    response.end(template);
 });
-app.listen(3000); // port 3000
+app.listen(3000);
