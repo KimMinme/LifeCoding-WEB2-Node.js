40f3ecb0ae5e149ac3d0b32feb2423a35de64253
KimMinme <128785019+KimMinme@users.noreply.github.com>
2023-07-14 21:15:18 +0900

    App - 함수를 이용해서 정리 정돈하기
    - template 반환 함수, list 반환 함수를 만들어 코드를 간결하게 만들었다.

--- main.js
+++ main.js
@@ -2,6 +2,36 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
+// title, list, body를 매개변수로 하여 template을 반환하는 함수
+function templateHTML(title, list, body){
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${body}
+  </body>
+  </html>
+  `;
+}
+
+// filelist를 매개변수로 하여 list를 반환하는 함수
+function templateList(filelist){
+  var list = '<ul>';
+  var i = 0;
+  while(i < filelist.length){
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+    i = i + 1;
+  }
+  list = list + '</ul>';
+  return list;
+}
+
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
@@ -9,67 +39,23 @@ var app = http.createServer(function(request,response){
 
     if(pathname === '/'){
       if(queryData.id === undefined){
-
         fs.readdir('./data',  function(error, filelist){
           var title = 'Welcome';
           var description = 'Hello, Node.js';
-          var list = '<ul>'; // 여는 <ul> 태그
-          var i = 0;
-          while(i < filelist.length){
-						// list 변수에 readdir 함수로 읽은 filelist 추가
-            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-            i = i + 1;
-          }
-          list = list + '</ul>'; // 닫는 <ul> 태그
-					// template 변수에 ${list} 추가
-          var template = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            ${list}
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-          </html>
-          `;
+					// list와 template 변수에 함수 반환값 저장
+          var list = templateList(filelist);
+          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
           response.writeHead(200);
           response.end(template);
         })
 
       } else{
         fs.readdir('./data',  function(error, filelist){
-          var title = 'Welcome';
-          var description = 'Hello, Node.js';
-          var list = '<ul>';
-          var i = 0;
-          while(i < filelist.length){
-            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-            i = i + 1;
-          }
-          list = list + '</ul>';
           fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
             var title = queryData.id;
-						// template 변수에 ${list} 추가
-            var template = `
-            <!doctype html>
-            <html>
-            <head>
-              <title>WEB1 - ${title}</title>
-              <meta charset="utf-8">
-            </head>
-            <body>
-              <h1><a href="/">WEB</a></h1>
-              ${list}
-              <h2>${title}</h2>
-              <p>${description}</p>
-            </body>
-            </html>
-            `;
+						// list와 template 변수에 함수 반환값 저장
+            var list = templateList(filelist);
+            var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
             response.writeHead(200);
             response.end(template);
           });
