6ce21aed056aae42d54272d71949b9802c279010
KimMinme <128785019+KimMinme@users.noreply.github.com>
2023-07-14 21:10:43 +0900

    App - Not found 오류 구현
    - pathname이 '/'(root)일 경우 페이지를 보여주고 그렇지 않을 경우 Not Found 메시지를 띄운다.

--- main.js
+++ main.js
@@ -5,37 +5,37 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
+		// pathname을 변수 pathname에 저장
+    var pathname = url.parse(_url, true).pathname;
     var title = queryData.id;
-    if(_url == '/'){
-      title = 'Welcome';
+
+    if(pathname === '/'){ // pathname이 root일 경우 페이지 보여줌
+      fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+        var template = `
+      <!doctype html>
+      <html>
+      <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        <ul>
+          <li><a href="/?id=HTML">HTML</a></li>
+          <li><a href="/?id=CSS">CSS</a></li>
+          <li><a href="/?id=JavaScript">JavaScript</a></li>
+        </ul>
+        <h2>${title}</h2>
+        <p>${description}</p>
+      </body>
+      </html>
+      `;
+      response.writeHead(200);
+      response.end(template);
+      });
+    } else{ // 아닐 경우 Not Found라는 메시지를 띄움
+      response.writeHead(404);
+      response.end('Not found');
     }
-    if(_url == '/favicon.ico'){
-      return response.writeHead(404);
-    }
-    response.writeHead(200);
-    // queryData.id와 일치하는 파일명의 파일을 읽어옴
-    // 읽은 파일이 저장된 description 변수
-    fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-      var template = `
-    <!doctype html>
-		<html>
-		<head>
-		  <title>WEB1 - ${title}</title>
-		  <meta charset="utf-8">
-		</head>
-		<body>
-		  <h1><a href="/">WEB</a></h1>
-		  <ul>
-		    <li><a href="/?id=HTML">HTML</a></li>
-		    <li><a href="/?id=CSS">CSS</a></li>
-		    <li><a href="/?id=JavaScript">JavaScript</a></li>
-		  </ul>
-		  <h2>${title}</h2>
-		  <p>${description}</p>
-		</body>
-		</html>
-    `;
-    response.end(template);
-    });
 });
 app.listen(3000);
