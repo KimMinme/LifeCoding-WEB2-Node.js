9ba186bb1712d383105f3de62fe05a7cd576f64b
KimMinme <minme3859@gmail.com>
2023-07-17 11:28:44 +0900

    App - 글삭제 기능 완성
    - fs.unlink() 함수를 통해 글을 삭제할 수 있게 되었다.

--- main.js
+++ main.js
@@ -55,10 +55,10 @@ var app = http.createServer(function(request,response){
           fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
             var title = queryData.id;
             var list = templateList(filelist);
-						// delete 버튼 구현
             var template = templateHTML(title, list, 
             `<h2>${title}</h2>${description}`, 
-            `<a href="/create">create</a> 
+            `
+            <a href="/create">create</a> 
             <a href="/update?id=${title}">update</a> 
             <form action="delete_process" method="post">
               <input type="hidden" name="id" value="${title}">
@@ -75,8 +75,9 @@ var app = http.createServer(function(request,response){
         var title = 'WEB - create';
         var description = 'Hello, Node.js';
         var list = templateList(filelist);
-        var template = templateHTML(title, list, `
-        <form action="/create_process" method="post">
+        var template = templateHTML(title, list, 
+        `
+        <form action="create_process" method="post">
           <p>
             <input type="text" name="title" placeholder="title">
           </p>
@@ -101,7 +102,7 @@ var app = http.createServer(function(request,response){
         var title = post.title;
         var description = post.description;
         fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-          response.writeHead(302, {Location: `/?id=${title}`});
+          response.writeHead(302, {Location: `/`});
           response.end();
         });
       });
@@ -142,9 +143,23 @@ var app = http.createServer(function(request,response){
         var description = post.description;
         fs.rename(`data/${id}`, `data/${title}`, function(error){
           fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-            response.writeHead(302, {Location: `/?id=${title}`});
+            response.writeHead(302, {Location: `/`});
             response.end();
           });
+        });
+      });
+      // fs.unlink() 함수를 이용하여 글삭제
+    } else if(pathname === '/delete_process'){
+      var body = '';
+      request.on('data', function(data){
+        body = body + data;
+      });
+      request.on('end', function(){
+        var post = qs.parse(body);
+        var id = post.id;
+        fs.unlink(`data/${id}`, function(error){
+          response.writeHead(302, {Location: `/`});
+          response.end();
         });
       });
     } else{
